38266c3842b1c5976423a6b732cef2b9dddc938d
AmirbekAsrorov <147371906+AmirbekAsrorov@users.noreply.github.com>
2024-03-16 00:03:37 +0500

    Update problem6.cpp

--- problem6.cpp
+++ problem6.cpp
@@ -3,25 +3,6 @@
 
 using namespace std;
 
-// int main() {
-//     int n;
-//     cout << "Enter the number of elements in the sequence: ";
-//     cin >> n;
-//     vector<int> sequence(n);
-//     cout << "Enter " << n << " numbers:" << endl;
-//     for (int i = 0; i < n; i++) {
-//         cin >> sequence[i];
-//     }for (int i = 0; i < n; i++){
-//         int count = 0;
-//         for (int j = 0; j < n; j++){
-//             if (sequence[j] < sequence[i]){
-//                 count++;
-//             }
-//         }
-//         cout << "For value " << sequence[i] << ", there are " << count << " smaller numbers."<<endl;
-//     }
-//     return 0;
-// }
 vector<int> countSmallerNumbers(const vector<int>& sequence) {
     int n = sequence.size();
     vector<int> counts(n, 0);
@@ -32,7 +13,6 @@ vector<int> countSmallerNumbers(const vector<int>& sequence) {
             }
         }
     }
-
     return counts;
 }
 int main() {
