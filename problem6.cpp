b90f71580907ff756d088d5fc2bcbcc01ff630c4
AmirbekAsrorov <147371906+AmirbekAsrorov@users.noreply.github.com>
2024-03-15 23:57:40 +0500

    Update problem6.cpp

--- problem6.cpp
+++ problem6.cpp
@@ -3,22 +3,63 @@
 
 using namespace std;
 
+// int main() {
+//     int n;
+//     cout << "Enter the number of elements in the sequence: ";
+//     cin >> n;
+//     vector<int> sequence(n);
+//     cout << "Enter " << n << " numbers:" << endl;
+//     for (int i = 0; i < n; i++) {
+//         cin >> sequence[i];
+//     }for (int i = 0; i < n; i++){
+//         int count = 0;
+//         for (int j = 0; j < n; j++){
+//             if (sequence[j] < sequence[i]){
+//                 count++;
+//             }
+//         }
+//         cout << "For value " << sequence[i] << ", there are " << count << " smaller numbers."<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+
+
+vector<int> countSmallerNumbers(const vector<int>& sequence) {
+    int n = sequence.size();
+    vector<int> counts(n, 0);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (sequence[j] < sequence[i]) {
+                counts[i]++;
+            }
+        }
+    }
+
+    return counts;
+}
+
 int main() {
     int n;
     cout << "Enter the number of elements in the sequence: ";
     cin >> n;
+
     vector<int> sequence(n);
-    cout << "Enter " << n << " numbers:" << endl;
+    cout << "Enter the sequence of numbers: ";
     for (int i = 0; i < n; i++) {
         cin >> sequence[i];
-    }for (int i = 0; i < n; i++){
-        int count = 0;
-        for (int j = 0; j < n; j++){
-            if (sequence[j] < sequence[i]){
-                count++;
-            }
-        }
-        cout << "For value " << sequence[i] << ", there are " << count << " smaller numbers."<<endl;
     }
+
+    vector<int> smallerCounts = countSmallerNumbers(sequence);
+
+    cout << "Number of smaller numbers for each value in the sequence:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << smallerCounts[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
